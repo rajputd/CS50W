ffd83762aca1fed251a4fa646136bafcb83234f8
Dileep Rajput <dileep_rajput@yahoo.com>
2018-12-17 11:19:29 -0500

    Document methods in User Model

--- project1/Models/User.py
+++ project1/Models/User.py
@@ -1,19 +1,29 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User:
+    """An object that provides access to the users table in this applications database."""
 
-    def __init__(self, conn, username, password):
+    def __init__(self, conn, username="", password=""):
+        """Returns an instance of the User Model.
+        conn is a SQL alchemy connection to the database and is required.
+        username and password are the users credentials and are optional.
+        """
         self.conn = conn
         self.username = username
         self.set_password(password)
 
     def set_password(self, password):
+        """Generates a hash of the given password and sets the password hash member field of
+        this object."""
         self.pw_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """Checks if the given (unhashed) password matches with the hash associated with the
+         current user."""
         return check_password_hash(self.pw_hash, password)
 
     def exists(self):
+        """Returns True if the user with the set username exists, false otherwise."""
         q = f"SELECT count(username) FROM users WHERE username='{self.username}' LIMIT 1"
         result = self.conn.execute(q).fetchall()
 
@@ -23,6 +33,8 @@ class User:
         return False
 
     def register(self):
+        """Inserts the given user into the table of know users. Throws an error if a database
+         issue occurs."""
         try:
             q = f"INSERT INTO users (username, password) VALUES ('{self.username}', '{self.pw_hash}')"
             self.conn.execute(q)
