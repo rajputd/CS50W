dc6366e0562ab7db52f36af9727c3349ef289ea7
Dileep Rajput <dileep_rajput@yahoo.com>
2019-01-22 18:49:23 -0500

    Move login code to seperate route and add comments

--- project2/application.py
+++ project2/application.py
@@ -14,24 +14,37 @@ users = []
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    #if user is loged in go to messages
     if 'handle' in session:
         return redirect(url_for('messages'))
 
+    #otherwise go to login form
+    return redirect(url_for('login'))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    #render login form if GET
     if request.method == "GET":
         return render_template("login.html", error="")
-    else:
-        handle = request.form.get("handle")
-        if handle in users:
-            return render_template("login.html", error="Sorry, that handle is currently taken")
-        else:
-            users.append(handle)
-            session['handle'] = handle
-            return redirect(url_for('messages'))
+
+    #render error if handle is taken
+    handle = request.form.get("handle")
+    if handle in users:
+        return render_template("login.html", error="Sorry, that handle is currently taken")
+
+    #register handle and associate user with it
+    users.append(handle)
+    session['handle'] = handle
+
+    return redirect(url_for('messages'))
 
 @app.route("/logout")
 def logout():
+    #unregister user
     users.remove(session["handle"])
     session.pop("handle", None)
+
     return redirect(url_for("index"))
 
 @app.route("/messages")
