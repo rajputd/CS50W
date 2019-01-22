d76beb012080dae011b2b5affd141c10b671b7ec
Dileep Rajput <dileep_rajput@yahoo.com>
2019-01-21 20:53:06 -0500

    Allow users to log into the application with their handles

--- project2/application.py
+++ project2/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -9,6 +9,19 @@ socketio = SocketIO(app)
 
 print("Active on http://localhost:5000")
 
-@app.route("/")
+users = []
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("login.html", error="")
+    if request.method == "GET":
+        return render_template("login.html", error="")
+    else:
+        if request.form.get("handle") in users:
+            return render_template("login.html", error="Sorry, that handle is currently taken")
+        else:
+            users.append(request.form.get("handle"))
+            return redirect(url_for('messages'))
+
+@app.route("/messages")
+def messages():
+    return render_template("messages.html")
