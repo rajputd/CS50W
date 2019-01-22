83b1966d274bfe976135888ebe159768cc54ac44
Dileep Rajput <dileep_rajput@yahoo.com>
2019-01-21 20:58:46 -0500

    Allow users to stay logged with cookies

--- project2/application.py
+++ project2/application.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
 
 print("Active on http://localhost:5000")
 
@@ -13,6 +14,9 @@ users = []
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    if 'handle' in session:
+        return redirect(url_for('messages'))
+
     if request.method == "GET":
         return render_template("login.html", error="")
     else:
@@ -20,6 +24,7 @@ def index():
             return render_template("login.html", error="Sorry, that handle is currently taken")
         else:
             users.append(request.form.get("handle"))
+            session['handle'] = request.form.get("handle")
             return redirect(url_for('messages'))
 
 @app.route("/messages")
