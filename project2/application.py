21310b5468391ea4ba464ef943c22e41a413c130
Dileep Rajput <dileep_rajput@yahoo.com>
2019-07-14 23:33:05 -0400

    Store chat logs in memory and refactor code

--- project2/application.py
+++ project2/application.py
@@ -7,17 +7,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app)
-
-print("Active on http://localhost:5000")
 
 users = []
 channel_logs = { 'General' : [] }
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    #if user is loged in go to messages
+    #if user is logged in go to messages
     if 'handle' in session:
         return redirect(url_for('messages'))
 
@@ -31,19 +27,13 @@ def login():
     if request.method == "GET":
         return render_template("login.html", error="")
 
-    #render error if handle is taken
+    #don't bother checking if handle is taken since its not in the project spec
     handle = request.form.get("handle")
-    if handle in users:
-        return render_template("login.html", error="Sorry, that handle is currently taken")
 
-    #register handle and associate user with it
-    users.append(handle)
+    #add handle to user cookie
     session['handle'] = handle
 
-    #add user to general channel
-    session['current_channel'] = 'General'
-
-    return redirect(url_for('messages'))
+    return redirect(url_for('messages', channel='General'))
 
 @app.route("/logout")
 def logout():
@@ -51,21 +41,17 @@ def logout():
     if session.get('handle') == None:
         redirect(url_for("login"))
 
-    #unregister user
-    users.remove(session["handle"])
     session.pop("handle", None)
 
     return redirect(url_for("index"))
 
-@app.route("/messages")
-def messages():
+@app.route("/messages/<string:channel>")
+def messages(channel):
 
     if session.get('handle') == None:
         return redirect(url_for("login"))
 
-    cur_channel = session['current_channel']
-    chatlog = channel_logs[cur_channel]
-    return render_template("messages.html", channel=cur_channel, chatlog=chatlog)
+    return render_template("messages.html", channel=channel, chatlog=channel_logs[channel])
 
 @socketio.on('connect')
 def handle_message():
@@ -74,11 +60,14 @@ def handle_message():
 
 @socketio.on('message')
 def handle_my_event(data):
-    print(data)
     data['sender'] = session['handle']
+    print(data)
+
+    #add message to channel logs
+    channel_name = data['channel']
+    channel_logs[channel_name].append(data)
 
     #add to channel's chat log
-    channel_logs[session['current_channel']].append(data)
     emit('broadcast_message', data, broadcast=True)
     return
 
