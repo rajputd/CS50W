10c1a977539b3bcb72ab9737ffe814493f912fbc
Dileep Rajput <dileeprajput686@gmail.com>
2022-03-11 17:13:14 -0500

    Add basic entry page

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
 from . import util
 
@@ -6,5 +7,13 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    if util.get_entry(title=title) == None:
+        return HttpResponseNotFound('<h1> 404 Error: page "' + title + '" not found')
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title
     })
 
