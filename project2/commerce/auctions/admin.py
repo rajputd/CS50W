4d66ebb39e05d14915ef18c9206f5d552fcc36ea
Dileep Rajput <dileeprajput686@gmail.com>
2022-03-12 10:06:53 -0500

    Add basic listing model

--- project2/commerce/auctions/admin.py
+++ project2/commerce/auctions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import AuctionListing
 
 # Register your models here.
+admin.site.register(AuctionListing)
--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    # watchlist of listings?
+    # list of created listings?
+    # list of bids
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+# TODO auction listing
+    # active
+    # category
+
+# TODO auction bid
+    # auction listing
+    # price
+    # user
+
+# TODO comments
+    # listing
+    # comments
+
+# TODO categories
+    # title
