5be75087c2bcefa01ac57ea2dd910df6e485c4e7
Dileep Rajput <dileeprajput686@gmail.com>
2022-03-13 20:36:33 -0400

    Add auction categories model

--- project2/commerce/auctions/admin.py
+++ project2/commerce/auctions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AuctionListing
+from .models import AuctionListing, AuctionListingCategory
 
 # Register your models here.
 admin.site.register(AuctionListing)
+admin.site.register(AuctionListingCategory)
--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -11,12 +11,19 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class AuctionListingCategory(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.id.__str__() + '-' + self.title
+
 class AuctionListing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(AuctionListingCategory, on_delete=models.CASCADE)
     create_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
